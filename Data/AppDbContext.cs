296a5088102130f6fb7560342b616d35a9e72a6c
Mostafa-sayed-lamlom <mostafasayedlamlom@gmail.com>
2024-03-27 13:02:23 +0200

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿
+namespace GameZone.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
